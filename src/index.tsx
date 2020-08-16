7504f005311024846902ea915285a11f92c0eb7f
竹中誠矢 <seiya@takenakamakotoyanoiMac.local>
2020-08-16 18:52:52 +0900

    フロント実装

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import firebase from 'firebase/app';
+
 import App from './App';
+import FirebaseApp from './FirebaseApp';
+import { ThemeContext } from './contexts';
+import mangarelTheme from './theme';
+import firebaseConfig from './firebase-config';
 import * as serviceWorker from './serviceWorker';
 
+import 'semantic-ui-css/semantic.min.css';
+import './index.css';
+
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <FirebaseApp>
+      <ThemeContext.Provider value={mangarelTheme}>
+        <App />
+      </ThemeContext.Provider>
+    </FirebaseApp>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
